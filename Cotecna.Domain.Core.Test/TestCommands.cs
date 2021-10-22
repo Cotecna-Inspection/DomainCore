9b5c960675484d990d66281b807060d2ede1dc5b
eltjo <eltjo.kraai@cotecna.es>
2021-10-22 08:56:48 +0200

    define behaviour

---
+++ Cotecna.Domain.Core.Test/TestCommands.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+
+namespace Cotecna.Domain.Core.Test
+{
+    public class Command : ICommand
+    {
+        public string Test { get; set; }
+    }
+
+    public class CommandHandler : ICommandHandler<Command>
+    {
+        public void Handle(Command command)
+        {
+            TestResult = command.Test;
+        }
+
+        public static string TestResult { get; set; }
+    }
+
+    public class AsyncCommandHandler : IAsyncCommandHandler<AsyncCommand>
+    {
+        public async Task HandleAsync(AsyncCommand command)
+        {
+            await Task.Delay(2);
+            TestResult = command.Test;
+        }
+
+        public static string TestResult { get; set; }
+    }
+
+    public class AsyncCommand : ICommand
+    {
+        public string Test { get; set; }
+    }
+
+}
